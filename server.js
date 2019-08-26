aceca1e8f54a78b52a9132db6ccda2eecd169ec5
Rishi Prasad <rishipr@umich.edu>
2019-08-26 14:34:56 -0400

    Deployed to Heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const projects = require("./routes/api/projects");
@@ -39,6 +40,16 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/projects", projects);
 app.use("/api/tasks", tasks);
+
+// Serve static assets (build folder) if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
